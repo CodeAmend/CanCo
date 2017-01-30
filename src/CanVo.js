0669ee33275b3cf11e0f7ca211d8ba95f52ff479
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-01-30 13:17:18 -0500

    Added public width and height variables
    This mainly keeps from needing to getCanvasElement()
    just to get the width/height.
    
    Also updated the setCanvasDim() so that this.width/height
    would only update if the canvas context did.

--- src/CanVo.js
+++ src/CanVo.js
@@ -6,19 +6,29 @@
     var CanVo = function(id, width, height) {
         canvas = document.getElementById(id);
         ctx = canvas.getContext('2d');
+
         this.setCanvasDim(width, height);
+
+        // Public variables
+        this.width = canvas.width;
+        this.height = canvas.height;
     };
 
     CanVo.prototype =  {
+
         paintBackground: function(color) {
             ctx.fillStyle = color;
             ctx.fillRect(0, 0, canvas.width, canvas.height)
         },
+
         setCanvasDim: function(width, height) {
-            canvas.width = width;
-            canvas.height = height;
+            // Keeps the update of public dimensions equal to canvas dimensions
+            if(width > 0) this.width = canvas.width = width;
+            if(height > 0) this.height = canvas.height = height;
+
             return this;
         },
+
         // These methods are added to make canvas and context private.
         getCanvasElement: function() {
             return canvas;

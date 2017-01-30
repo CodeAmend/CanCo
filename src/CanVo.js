09f0c677f3afdc24dec7990e925138d18c3ddbec
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-01-30 12:38:34 -0500

    made canvas and context private

--- src/CanVo.js
+++ src/CanVo.js
@@ -1,22 +1,25 @@
 (function(global){
 
+    // Private variables for ONLY this IIFE context.
+    var canvas, ctx;
+
     var CanVo = function(id, width, height) {
-        this.canvas = document.getElementById(id);
-        this.ctx = this.canvas.getContext('2d');
+        canvas = document.getElementById(id);
+        ctx = canvas.getContext('2d');
         this.setCanvasDim(width, height);
-    }
+    };
 
     CanVo.prototype =  {
         paintBackground: function(color) {
-            this.ctx.fillStyle = color;
-            this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
+            ctx.fillStyle = color;
+            ctx.fillRect(0, 0, canvas.width, canvas.height)
         },
         setCanvasDim: function(width, height) {
-            this.canvas.width = width;
-            this.canvas.height = height;
+            canvas.width = width;
+            canvas.height = height;
             return this;
         }
-    }
+    };
 
     global.CanVo = CanVo;
 

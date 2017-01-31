335a694a986b5084ab523c39a12b2b59bb3227d7
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-01-31 10:56:32 -0500

    Added setGridColsRows() and PVector()
    setGridColsRows() uses the width and height of canvas to calulate the
    row and column lengths. It is automatically ran during CanVo object
    creation. YOu can override the grid size at any time.
    
    PVector is just a vector object that has a 2d x and y point. Currently
    add() alows you to add the vecotrs easy such as v1.add(v2).

--- src/CanVo.js
+++ src/CanVo.js
@@ -10,22 +10,22 @@
         ctx = canvas.getContext('2d');
 
         this.setCanvasDim(width, height);
+        this.setGridColsRows(width, height);
 
         // Public variables and initializers
         this.width = canvas.width;
         this.height = canvas.height;
 
-        cols = rows = colSize = rowSize = null;
-
     };
 
     CanVo.prototype =  {
-
+        // Fills entire background of width and height
         paintBackground: function(color) {
             ctx.fillStyle = color;
             ctx.fillRect(0, 0, canvas.width, canvas.height)
         },
 
+        // Changes the dimensions of entire canvas element.
         setCanvasDim: function(width, height) {
             // Keeps the update of public dimensions equal to canvas dimensions
             if(width > 0) this.width = canvas.width = width;
@@ -34,25 +34,31 @@
             return this;
         },
 
+        // An easier way to draw a circle by point, size and color.
         paintCircle: function(x, y, diameter, color ) {
             ctx.beginPath();
             ctx.fillStyle = color;
             ctx.arc(x, y, diameter / 2, 0, Math.PI * 2);
             ctx.fill();
         },
-        setGridSize: function(columnCount, rowCount) {
-            if(Number(columnCount) && columnCount > 0 && columnCount < canvas.width) {
+
+        // Users size of element and calculates grid size based off column and row count.
+        setGridColsRows: function(columnCount, rowCount) {
+            if(Number(columnCount) && columnCount > 0 && columnCount <= canvas.width) {
                 colSize = this.width / columnCount;
                 cols = columnCount;
             } else {
-                throw RangeError(columnCount + " is not valid.");
+                throw RangeError(columnCount + " is not a valid col count.");
             }
-            if(Number(rowCount) && rowCount > 0 && rowCount < canvas.height) {
+            if(Number(rowCount) && rowCount > 0 && rowCount <= canvas.height) {
                 rowSize = this.height / rowCount;
                 rows = rowCount;
             } else {
-                throw RangeError(rowCount + " is not valid.");
+                throw RangeError(rowCount + " is not valid row count.");
             }
+        },
+        createPVector: function(x, y) {
+            return new PVector(x, y)
         },
 
         // These methods are added to make canvas and context private.
@@ -69,6 +75,16 @@
         cols: function() { return cols },
         rows: function() { return rows }
     };
+
+    function PVector(x, y) {
+        this.x = x;
+        this.y = y;
+        this.add = function(pVector) {
+            x = pVector.x + this.x;
+            y = pVector.y + this.y;
+            return new PVector(x, y);
+        }
+    }
 
     global.CanVo = CanVo;
 

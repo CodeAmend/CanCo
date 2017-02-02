548423315fe177402b05332523409258612efee1
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-02-02 17:19:56 -0500

    Walker updated so move() takes a funtion algorith
    script just had code commented out (for testing walker)
    index, js links fixed.

--- examples/Walker.js
+++ examples/Walker.js
@@ -2,40 +2,52 @@
  * Created by codeamend on 1/30/17.
  */
 
-var board = new CanVo("canvas_id", 600, 400);
-board.paintBackground("red");
+var html = document.getElementsByTagName('html')[0];
+var board = new CanVo("canvas_id", html.clientWidth, html.clientHeight);
 
-var centerOfCanvas = board.createPVector(board.width / 2, board.height / 2);
-
+window.addEventListener('resize', function(e) {
+    board.setCanvasDim(html.clientWidth, html.clientHeight);
+    walker.position = board.getCenterOfCanvas();
+});
 
 var Walker = {
     // centerOfCanvas: a PVector created by half of width and height.
-    position: centerOfCanvas,
-    diameter: 4,
-    stride: 5,
+    position: board.getCenterOfCanvas(),
+    diameter: 5,
+    stride: 6,
     color: "white",
+
     move: function() {
-        // Random number only -1 0 or 1
-        var moveX = Math.floor(Math.random() * 3) - 1;
-        var moveY = Math.floor(Math.random() * 3) - 1;
-        // stride * movement = negative zero or positive.
-        this.position.x += (this.stride  * moveY);
-        this.position.y += (this.stride  * moveX);
+        randDir(this);
     },
+
     draw: function() {
-        board.paintCircle(this.position.x, this.position.y, this.diameter, "white");
+        board.paintCircle(this.position.x, this.position.y, this.diameter, this.color);
     }
 
 };
 
+function randDir(walkerObject) {
+    walkerObject.position.addFunction(
+        function(item) {
+           var dir = Math.floor(Math.random() * 3) - 1;
+           return item + walkerObject.stride * dir;
+        }.bind(walkerObject)
+    )
+}
+
 var walker = Walker;
+
+
 function start() {
-    var framesPerSecond = 30;
+    var framesPerSecond = 60;
     setInterval(update, 1000 / framesPerSecond);
 }
 function update() {
+
     walker.draw();
     walker.move();
+
 }
 
 start();
--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
-var board = new CanVo("canvas_id", 600, 400);
-
-board.setGridSize(6, 4);
-
-console.log(board.cols() === 6);
-console.log(board.colSize() === 600 / 6);
-console.log(board.rows() === 4);
-console.log(board.rowSize() === 400 / 4);
-
-board.paintBackground("gray");
+// var board = new CanVo("canvas_id", 600, 400);
+//
+// board.setGridSize(6, 4);
+//
+// console.log(board.cols() === 6);
+// console.log(board.colSize() === 600 / 6);
+// console.log(board.rows() === 4);
+// console.log(board.rowSize() === 400 / 4);
+//
+// board.paintBackground("gray");
